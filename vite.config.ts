68daf0294d8adbae970a7ef25966672f495897e2
mohammadadilb87-design <mohammadadilb87@gmail.com>
2025-12-31 08:59:52 -0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+const API_URL = "https://alzcare-1.onrender.com";
+export default API_URL;
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
